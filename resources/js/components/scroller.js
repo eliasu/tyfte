06767202895001b2f41c1b86111ce95717ed9c68
Elias <elias@commandg.de>
2023-05-31 19:10:54 +0200

    added vertical scrolling deleted everything related to horicontal scrolling

--- resources/js/components/scroller.js
+++ resources/js/components/scroller.js
@@ -26,10 +26,53 @@ export default function initScroller() {
     let fxWrapper = document.getElementById("section-fx");
 
     // initialize first FX on load: setFX(effectToStart, effectToEnd)
-    setFx(0,99)
+    // setFx(0,99)
 
     // add enter/leave callbacks
-   // TODO
+    // TODO
+
+    gsap.utils.toArray("[data-section]").forEach(function(elem) {
+        
+        ScrollTrigger.create({
+            trigger: elem,
+            markers: true,
+            onEnter: (i,el) => { console.log(`onEnter Section: ${getValue(i)}`) }, 
+            onEnterBack: (i,el) => { console.log(`onEnterBack Section: ${getValue(i)}`) }, 
+            // onLeave: (i,el) => { console.log(`onLeave Section: ${getValue(i)}`) }, 
+            // onLeaveBack: (i,el) => { console.log(`onLeaveBack Section: ${getValue(i)}`) }, 
+        });
+    });
+
+    function getValue(elem) {
+        return elem.trigger.querySelector("[data-content]").dataset.content
+    }
+
+    // setup scrollTrigger
+    // let scrollTimeline = gsap.timeline({
+    //     // yes, we can add it to an entire timeline!
+    //     scrollTrigger: {
+    //       trigger: "main",
+    //     //   pin: true,   // pin the trigger element while active
+    //     //   start: "top top", // when the top of the trigger hits the top of the viewport
+    //     //   end: "+=500", // end after scrolling 500px beyond the start
+    //       scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+    //     //   snap: {
+    //     //     snapTo: "labels", // snap to the closest label in the timeline
+    //     //     duration: {min: 0.2, max: 3}, // the snap animation should be at least 0.2 seconds, but no more than 3 seconds (determined by velocity)
+    //     //     delay: 0.2, // wait 0.2 seconds from the last scroll event before doing the snapping
+    //     //     ease: "power1.inOut" // the ease of the snap animation ("power3" by default)
+    //     //   }
+    //     }
+    //   });
+    
+    // add animations and labels to the timeline
+    // scrollTimeline.addLabel("start")
+    //   .from(".box p", {scale: 0.3, rotation:45, autoAlpha: 0})
+    //   .addLabel("color")
+    //   .from(".box", {backgroundColor: "#28a92b"})
+    //   .addLabel("spin")
+    //   .to(".box", {rotation: 360})
+    //   .addLabel("end");
 
     function setFx(sectionIndex, sectionIndexPrev) {
         console.log(`setFX: Activate section${sectionIndex} and deactivate section${sectionIndexPrev}`)
