f3d933d3f30a0cc279cbd231cf0a7cfb6abb0708
Hannes Hoepfner <hello@hanneshoepfner.io>
2023-06-16 16:14:16 +0200

    swiper in place

---
+++ resources/js/components/swiper.js
@@ -0,0 +1,31 @@
+ // import Swiper JS
+//  import Swiper from 'swiper';
+ import Swiper from "swiper/bundle";
+ // import Swiper styles
+//  import 'swiper/css';
+ import "swiper/css/bundle";
+
+ const swiper = new Swiper(
+  '.swiper',
+  {
+   // Optional parameters
+  //  direction: 'vertical',
+   loop: true,
+ 
+   // If we need pagination
+   pagination: {
+     el: '.swiper-pagination',
+   },
+ 
+   // Navigation arrows
+   navigation: {
+     nextEl: '.swiper-button-next',
+     prevEl: '.swiper-button-prev',
+   },
+ 
+   // And if we need scrollbar
+   scrollbar: {
+     el: '.swiper-scrollbar',
+   },
+  }
+ );
