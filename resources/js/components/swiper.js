1dd078d573846dd1dd920d1ec02d433821b217d6
Hannes Hoepfner <hello@hanneshoepfner.io>
2023-06-21 10:28:02 +0200

    basic working

--- resources/js/components/swiper.js
+++ resources/js/components/swiper.js
@@ -1,34 +1,27 @@
- // import Swiper JS
-//  import Swiper from 'swiper';
- import Swiper from "swiper/bundle";
- // import Swiper styles
-//  import 'swiper/css';
- import "swiper/css/bundle";
 
- const swiper = new Swiper(
-  '.swiper',
-  {
-   // Optional parameters
-  //  direction: 'vertical',
-   loop: true,
- 
-   // If we need pagination
-   pagination: {
-     el: '.swiper-pagination',
-   },
- 
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle';
+
+// import styles bundle
+import 'swiper/css/bundle';
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1.3,
+  loop: true,
+  spaceBetween: 30,
+  centeredSlides: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
    // Navigation arrows
    navigation: {
-     nextEl: '.swiper-button-next',
-     prevEl: '.swiper-button-prev',
-   },
- 
-   // And if we need scrollbar
-   scrollbar: {
-     el: '.swiper-scrollbar',
-   },
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 
-
-      centeredSlides: true,
-  }
- );
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
