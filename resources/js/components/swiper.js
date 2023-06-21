eb23fc7e677d684f83eabb30344d112d27a162df
Hannes Hoepfner <hello@hanneshoepfner.io>
2023-06-21 11:34:35 +0200

    creative effect

--- resources/js/components/swiper.js
+++ resources/js/components/swiper.js
@@ -1,27 +1,37 @@
-
 // import Swiper bundle with all modules installed
-import Swiper from 'swiper/bundle';
+import Swiper from "swiper/bundle";
 
 // import styles bundle
 // import 'swiper/css/bundle';
 
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 1.3,
-  loop: true,
-  spaceBetween: 30,
-  centeredSlides: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-   // Navigation arrows
-   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+    slidesPerView: 1.3,
+    loop: true,
+    spaceBetween: 30,
+    centeredSlides: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    // Navigation arrows
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
 
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
+    // And if we need scrollbar
+    scrollbar: {
+        el: ".swiper-scrollbar",
+    },
+
+    effect: "creative",
+    creativeEffect: {
+        prev: {
+            shadow: true,
+            translate: [0, 0, -400],
+        },
+        next: {
+            translate: ["100%", 0, 0],
+        },
+    },
 });
