175bfffa71527aaaee918b5360f45736dc3cc5aa
Elias <elias@commandg.de>
2023-11-07 10:42:54 +0100

    fixed safari blendmode issues via "translate3d"

--- tailwind.config.tyfte.js
+++ tailwind.config.tyfte.js
@@ -150,6 +150,17 @@ module.exports = {
   plugins: [
     plugin(function({ addVariant }) {
       addVariant('touch', '@media (hover: none) and (pointer: coarse)')
+    }), 
+    plugin(function({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          'translate-z': (value) => ({
+            '--tw-translate-z': value,
+            transform: ` translate3d(var(--tw-translate-x), var(--tw-translate-y), var(--tw-translate-z)) rotate(var(--tw-rotate)) skewX(var(--tw-skew-x)) skewY(var(--tw-skew-y)) scaleX(var(--tw-scale-x)) scaleY(var(--tw-scale-y))`,
+          }), // this is actual CSS
+        },
+        { values: theme('translate'), supportsNegativeValues: true }
+      )
     })
   ]
 }
