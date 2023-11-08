8efdd4749895c2bf37ab16862b84b166b913da58
Elias <elias@commandg.de>
2023-11-08 10:36:18 +0100

    added hls js file with import

---
+++ resources/js/components/hls.js
@@ -0,0 +1,11 @@
+/**
+ * hls component
+ * import with "import initHLS from './components/hls';"
+**/
+
+import Hls from 'hls.js'
+
+export default function initHLS() { 
+    console.log("** init hls from /components/hls.js **")
+    
+}   
