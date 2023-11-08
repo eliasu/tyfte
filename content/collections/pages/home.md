a8a686b9c137091062fe3e8e88005cc5b9d0ba64
Elias <elias@commandg.de>
2023-11-08 19:49:05 +0100

    added hls instead of dash

--- content/collections/pages/home.md
+++ content/collections/pages/home.md
@@ -10,7 +10,7 @@ sitemap_change_frequency: weekly
 sitemap_priority: 0.5
 template: '@blueprint'
 updated_by: 06410ee8-92e1-45e7-b0e3-9fd7e0400442
-updated_at: 1698798167
+updated_at: 1699454501
 teaser:
   -
     type: paragraph
@@ -68,7 +68,6 @@ teaser:
       -
         type: text
         text: 'our wizards have it all covered – from information architecture and implementation to hosting and beyond. Let’s fry!'
-background_video: videos/backgrounds/bg_2/tyfte_bg_2.mpd
 accent_color: '#C23E3E'
 video_color: '#8F1650'
 ---
--- resources/js/components/hls.js
+++ resources/js/components/hls.js
@@ -8,4 +8,63 @@ import Hls from 'hls.js'
 export default function initHLS() { 
     console.log("** init hls from /components/hls.js **")
     
+    // Function to handle the video loading process
+    function loadVideo(videoElement) {
+        const videoSrc = videoElement.getAttribute('data-hls');
+        
+        // Check if the data-hls-lazy attribute is "false"
+        const lazyLoad = videoElement.getAttribute('data-hls-lazy') === 'false';
+
+        // If data-hls-lazy is "false" or the video is in view, load the video
+        if (lazyLoad || isElementInViewport(videoElement)) {
+            if (videoElement.canPlayType('application/vnd.apple.mpegurl')) {
+                videoElement.src = videoSrc;
+                console.log("##########")
+                console.log("loaded element:")
+                console.log(videoElement)
+                console.log("##########")
+                console.log("##########")
+            } else if (Hls.isSupported()) {
+                const hls = new Hls();
+                hls.loadSource(videoSrc);
+                hls.attachMedia(videoElement);
+                console.log("##########")
+                console.log("loaded element:")
+                console.log(videoElement)
+                console.log("##########")
+                console.log("##########")
+            }
+        }
+    }
+
+    // Function to check if an element is in the viewport
+    function isElementInViewport(element) {
+        const rect = element.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
+
+    // Find all video elements with data-hls attribute
+    const videoElements = document.querySelectorAll('video[data-hls]');
+
+    // Create Intersection Observer for each video element
+    videoElements.forEach((videoElement) => {
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    // Load the video when it's in view
+                    loadVideo(videoElement);
+                    // Stop observing after loading
+                    observer.disconnect();
+                }
+            });
+        });
+
+        // Start observing each video element
+        observer.observe(videoElement);
+    });
 }   
